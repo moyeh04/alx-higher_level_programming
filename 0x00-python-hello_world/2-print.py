ee3cec25e77d07d3f380a7d0c56e79717d75a715
Mohamed Yehia <orzamsok@gmail.com>
2025-03-01 17:27:17 +0200

    2. Hello, print
    Write a Python script that prints exactly "Programming is like building
    a multilingual puzzle, followed by a new line.
    
    Use the function print
    
    guillaume@ubuntu:~/py/0x00$ ./2-print.py
    
    "Programming is like building a multilingual puzzle
    
    guillaume@ubuntu:~/py/0x00$

---
+++ 0x00-python-hello_world/2-print.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# A Python script that prints exactly "Programming is like building a
+# multilingual puzzle, followed by a new line.
+print('"Programming is like building a multilingual puzzle')
