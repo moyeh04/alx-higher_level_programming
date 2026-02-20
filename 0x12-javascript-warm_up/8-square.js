9425493958112dad07f47a687921914e4c369148
Mohamed Yehia <orzamsok@gmail.com>
2026-02-20 22:28:54 +0200

    8. Square
    Write a script that prints a square
    
    The first argument is the size of the square
    
    If the first argument can’t be converted to an integer, print “Missing
    size”
    
    You must use the character X to print the square
    
    You must use console.log(...) to print all output
    
    You are not allowed to use var
    
    You must use a loop (while, for, etc.)
    
    guillaume@ubuntu:~/0x12$ ./8-square.js
    
    Missing size
    
    guillaume@ubuntu:~/0x12$ ./8-square.js School
    
    Missing size
    
    guillaume@ubuntu:~/0x12$ ./8-square.js 2
    
    XX
    
    XX
    
    guillaume@ubuntu:~/0x12$ ./8-square.js 6
    
    XXXXXX
    
    XXXXXX
    
    XXXXXX
    
    XXXXXX
    
    XXXXXX
    
    XXXXXX
    
    guillaume@ubuntu:~/0x12$ ./8-square.js -3
    
    guillaume@ubuntu:~/0x12$

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const num = parseInt(process.argv[2], 10);
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < num; i++) {
+    for (let j = 0; j < num; j++) {
+      process.stdout.write('X');
+    }
+    console.log('');
+  }
+}
