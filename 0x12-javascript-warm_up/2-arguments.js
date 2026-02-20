aaef12ff1973345b7b558d35a3101736ad2a37d3
Mohamed Yehia <orzamsok@gmail.com>
2026-02-20 07:19:47 +0200

    2. Optimize
    Utilize the .length attribute of process.argv so it can be like argc in
    C.

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,10 @@
 #!/usr/bin/node
-if (process.argv[3] !== undefined) {
-  console.log('Arguments found');
-} else if (process.argv[2] !== undefined) {
+const argc = process.argv.length - 2; // subtract (node binary + script path) = user args only
+
+if (argc === 0) {
+  console.log('No argument');
+} else if (argc === 1) {
   console.log('Argument found');
 } else {
-  console.log('No argument');
+  console.log('Arguments found');
 }
