09a111af083e193bedc979b22a4fff7da4bc02f6
Mohamed Yehia <orzamsok@gmail.com>
2026-02-24 21:11:35 +0200

    11. Make the script more robust

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 
-const argv = process.argv.slice(2).map(Number);
-if (argv.length < 2) console.log(0);
-else {
+const argv = process.argv
+  .slice(2)
+  .map(Number)
+  .filter((n) => Number.isFinite(n));
+
+if (argv.length < 2) {
+  console.log(0);
+} else {
   console.log(argv.sort((a, b) => b - a).slice(1, 2)[0]);
 }
 
