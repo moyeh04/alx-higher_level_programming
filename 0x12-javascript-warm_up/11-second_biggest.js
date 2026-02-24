84aeccd781c60673b9bcc29ba68ee3e52dbbe8fb
Mohamed Yehia <orzamsok@gmail.com>
2026-02-24 11:04:20 +0200

    11. Second biggest!
    Write a script that searches the second biggest integer in the list of
    arguments.
    
    You can assume all arguments can be converted to integer
    If no argument passed, print 0
    If the number of arguments is 1, print 0
    You must use console.log(...) to print all output
    You are not allowed to use var
    guillaume@ubuntu:~/0x12$ ./11-second_biggest.js
    0
    guillaume@ubuntu:~/0x12$ ./11-second_biggest.js 1
    0
    guillaume@ubuntu:~/0x12$ ./11-second_biggest.js 4 2 5 3 0 -3
    4
    guillaume@ubuntu:~/0x12$

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argv = process.argv.slice(2).map(Number);
+if (argv.length < 2) console.log(0);
+else {
+  console.log(argv.sort((a, b) => b - a).slice(1, 2)[0]);
+}
+
+// Another Solution:
+// argv.sort((a, b) => b - a);
+// console.log(argv[1]);
